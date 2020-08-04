3bdcaa19a5fbe5a6e9633b9b95ed77892286e20d
08prabhjot <prabhjot3737@gmail.com>
2020-08-04 12:08:55 -0700

    creating new migration

---
+++ db/migrate/20200804190212_participants_and_start_time.rb
@@ -0,0 +1,6 @@
+class ParticipantsAndStartTime < ActiveRecord::Migration[6.0]
+  def change
+    add_column :events, :participants, :Integer
+    add_column :events, :start_time, :datetime
+  end
+end
