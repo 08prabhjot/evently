6333f071ba07836fe610c084bd012d2d94134430
08prabhjot <prabhjot3737@gmail.com>
2020-08-04 11:10:36 -0700

    creating new model

---
+++ app/models/event.rb
@@ -0,0 +1,13 @@
+class Event < ApplicationRecord
+    before_validation :set_default_price
+
+    validates :title, presence: true, uniquness: true
+    validates :description, presence: true, length: {minimum: 100}
+    validates :location, presence: true
+    validates :price, presence: true
+    validates :image, presence: true
+    private
+    def set_default_price
+        self.price ||=0
+    end
+end
