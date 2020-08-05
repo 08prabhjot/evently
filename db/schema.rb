0c990806becda908cfd2cf26b0d11a565fd8a93b
08prabhjot <prabhjot3737@gmail.com>
2020-08-04 19:56:41 -0700

    adding new model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_190212) do
+ActiveRecord::Schema.define(version: 2020_08_04_231332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2020_08_04_190212) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "participants"
     t.datetime "start_time"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
